af89b103a54a86ae7aa75ae647930bea55f0a24e
rahul6611 <vekariyarahul661@gmail.com>
2024-03-20 20:25:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+import ReactPaginate from 'react-paginate';
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+function Items({ currentItems }) {
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((item) => (
+          <div>
+            <h3>Item #{item}</h3>
+          </div>
+        ))}
+    </>
+  );
+}
+
+function App() {
+
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const endOffset = itemOffset + 5;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / 5);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * 5) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+       <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  )
+}
+
+export default App
